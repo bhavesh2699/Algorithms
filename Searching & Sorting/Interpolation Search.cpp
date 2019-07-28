4e41009a54962d5b0fa66f0441d57c9ce99b5208
bs185_000@laptop <bs1852985@gmail.com>
2019-07-28 23:42:55 +0530

    Interpolation tested

--- Searching & Sorting/Interpolation Search.cpp
+++ Searching & Sorting/Interpolation Search.cpp
@@ -3,26 +3,35 @@
 #include <cmath>
 
 using namespace std;
-int jump(int arr[],int num,int size){
-	int end=sqrt(size);
-	int start=0;
-	while(arr[end]<=num && end<size)
+int inter(int arr[],int num,int size)
+{
+	int start=0,end=size-1;
+	while(start<=end && arr[start]<=num && arr[end]>=num)
 	{
-		start=end;
-		end+=sqrt(size);
+		if (start == end) 
+        { 
+            if (arr[start] == num) return start; 
+            return -1; 
+        } 
+       
+        int pos = start + (((double)(end - start)*(num - arr[start]) / 
+            (arr[end] - arr[start]))); 
+  
+        if (arr[pos] == num) 
+            return pos; 
+  
+        if (arr[pos] < num) 
+            start = pos + 1; 
+  
+        else
+            end = pos - 1;
 	}
-	for(int i=start;i<end;i++)
-	{
-		if(arr[i]==num)
-		{
-			return i;
-		}
-	}
+	return -1;
 	
 }
 int main() {
 	int i,n,num;
-  cout<<"enter number of elemnts in array:"<<endl;
+  cout<<"enter number of elements in array:"<<endl;
   cin>>n;
   int arr[n];
   for(i=0;i<n;i++)
@@ -32,7 +41,7 @@ int main() {
   cout<<"enter number to find in array:"<<endl;
   cin>>num;
   int size = sizeof(arr) / sizeof(arr[0]);
-  int finder=jump(arr,num,size);
+  int finder=inter(arr,num,size);
   cout<<"Element"<<num<<"Found at:"<<finder;
   
   return 0;

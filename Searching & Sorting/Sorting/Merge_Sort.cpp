ca218de8c24e97202a7f3ae3a5e2b9781422289c
bs185_000@laptop <bs1852985@gmail.com>
2019-08-14 19:05:48 +0530

    Merge Sort

---
+++ Searching & Sorting/Sorting/Merge_Sort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+void merge(int arr,int l,int m,int h)
+{
+	
+}
+void merge(int arr[],int l,int h)
+{   int mid=(l+h)/2;
+	if(l<h)
+	{
+		merge(arr,l,h);
+		merge(arr,mid+1,h);
+		mergeSort(arr,l,mid,h);
+	}
+	
+}
+int main()
+{   int n,s;
+	cout<<"enter size of array";
+	cin>>n;
+	int *arr=new int[n];
+	cout<<"enter values";
+	for(int i =0;i<n;i++)
+	{   cin>>s;
+		arr[i]=s;
+	}
+	merge(arr,0,n-1);
+	for(int i=0;i<n;i++)
+	{
+		cout<<arr[i];
+	}
+}

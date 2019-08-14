01873bdf644a2dc37465ad1bf62d157769874085
bs185_000@laptop <bs1852985@gmail.com>
2019-08-14 20:39:45 +0530

    Merge Sort

--- Searching & Sorting/Sorting/Merge_Sort.cpp
+++ Searching & Sorting/Sorting/Merge_Sort.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 #include<bits/stdc++.h>
 using namespace std;
-void merge(int arr,int l,int m,int h)
+void mergeSort(int arr[],int l,int m,int h)
 {
 	int k1=m-l+1;
-	int k2=m+h;
+	int k2=h-m;
 	int i,j,k;
 	int L[k1],R[k2];
 	for(i=0;i<k1;i++)
@@ -12,21 +12,47 @@ void merge(int arr,int l,int m,int h)
 	 for(j=0;j<k1;j++)
 	 R[k2]=arr[m+1+j];
 	 
-	while()
+	 i=0,j=0,k=l;
+	 
+	while(i<k1 && j<k2)
 	{
-		
+		if(L[i]<=R[j])
+		{
+			arr[k]=L[i];
+			i++;
+		}
+		else{
+			arr[k]=R[j];
+			j++;
+		}
+		k++;
 	}
+	while (i < k1) 
+    { 
+        arr[k1] = L[i]; 
+        i++; 
+        k++; 
+    } 
+  
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < k2) 
+    { 
+        arr[k2] = R[j]; 
+        j++; 
+        k++; 
+    } 
 }
 void merge(int arr[],int l,int h)
-{   int mid=(l+h)/2;
+{   
 	if(l<h)
-	{
-		merge(arr,l,h);
+	{   int mid=l+(h-l)/2;
+		merge(arr,l,mid);
 		merge(arr,mid+1,h);
 		mergeSort(arr,l,mid,h);
 	}
-	
 }
+
 int main()
 {   int n,s;
 	cout<<"enter size of array";

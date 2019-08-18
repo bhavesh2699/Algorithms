a6c2b4b98daaeae225a6d017a70c3e786a81a7af
bs185_000@laptop <bs1852985@gmail.com>
2019-08-18 13:07:15 +0530

    Island Problem using DFS

---
+++ Island-problem.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define R 5
+#define C 5
+using namespace std;
+int main()
+{
+	int arr[R][C]={ { 1, 1, 0, 0, 0 }, 
+                     { 0, 1, 0, 0, 1 }, 
+                     { 1, 0, 0, 1, 1 }, 
+                     { 0, 0, 0, 0, 0 }, 
+                     { 1, 0, 1, 0, 1 } }; 
+}
